f2793d3aadc3933f13df4c879902883c1b50ff93
silentWraith13 <sangwanshrey01@gmail.com>
2024-06-03 21:46:17 -0500

    Update App.cpp
    Some minor code refactoring.

--- Code/Game/App.cpp
+++ Code/Game/App.cpp
@@ -195,6 +195,7 @@ void App::BeginFrame()
 	g_theWindow->BeginFrame();
 	g_theRenderer->BeginFrame();
 	g_theJobSystem->BeginFrame();
+	
 	ImGui_ImplDX11_NewFrame();
 	ImGui_ImplWin32_NewFrame();
 	ImGui::NewFrame();
